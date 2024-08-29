09b6d0d2c6d59efbe25e46024982dd12b8f95ddc
daniel@bvm.network <daniel@bvm.network>
2024-08-29 16:41:29 +0700

    add missing files

---
+++ src/utils/helpers.cu
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <helpers.cuh>
+
+void printmat3d(long long* mat, int h, int w, int c)
+{
+    for (int i = 0; i < h; ++i)
+    {
+        for (int j = 0; j < w; ++j)
+        {
+            printf("[");
+            for (int k = 0; k < c; ++k)
+            {
+                printf("%lf ", 1.0f * mat[i * w * c + j * c + k] / (1LL << 32));
+            }
+            printf("], ");
+        }
+        printf("\n");
+    }
+}
+
+
+bool cuda_fmt_error(const cudaError_t &a)
+{
+    if (a != cudaSuccess)
+    {
+
+#if defined(LOGGING_DEBUG)
+        std::cerr << "CUDA Error: " << a << std::endl;
+#endif
+
+        return false;
+    }
+
+    return true;
+}
+
+std::ostream &operator << (std::ostream &s, const cudaError_t &a)
+{
+#if defined(LOGGING_VERBOSE)
+    const char* error = cudaGetErrorString(a);
+    s << error;
+#else
+    if (a != cudaSuccess)
+        s << int(a);
+#endif // LOGGING_VERBOSE
+
+    return s;
+}
+
+std::ostream &operator << (std::ostream &s, const FixedLongLong::FixedLongLongType &a) {
+	return s << 1.0f * a.value / FixedLongLong::ONE;
+};
